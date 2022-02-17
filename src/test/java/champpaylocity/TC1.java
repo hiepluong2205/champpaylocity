a556b1ec7a0c563c295674afb5705480039bfef6
hiepluong2205@gmail.com <hiepluong2205@gmail.com>
2022-02-17 14:40:52 -0800

    misc.

--- src/test/java/champpaylocity/TC1.java
+++ src/test/java/champpaylocity/TC1.java
@@ -1,12 +1,13 @@
 package champpaylocity;
 
+import com.github.javafaker.Faker;
 import org.testng.annotations.Test;
 
 public class TC1 {
     @Test
     public void test1() {
         String url = "https://wmxrwq14uc.execute-api.us-east-1.amazonaws.com/Prod/Account/Login", username = "TestUser155", password = "QkdRg|0R#_)r", authCode = "Basixc VGVzdFVzZXIxNTU6UWtkUmd8MFIjXyly";
-PageEmployeeDetails
-
+        System.out.println("Hello");
+        Faker faker = new Faker();
     }
 }
